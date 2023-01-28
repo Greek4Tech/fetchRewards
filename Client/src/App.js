13d63b9db121ab35a45183a3cb7ca1770f7bab2b
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-01-28 08:24:32 -0600

    styling

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from '../src/components/Form';  // assuming Form.js is in the same directory as app.js or index.js
+import './App.css';
 
 function App() {
   return (
