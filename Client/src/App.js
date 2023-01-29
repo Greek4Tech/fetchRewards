6fc3585950304827b775ffb68d540cd5af2be701
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-01-29 15:01:33 -0600

    Bootstrap

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from '../src/components/Form';  // assuming Form.js is in the same directory as app.js or index.js
 import './App.css';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
+      {/* <Nav /> */}
       <Form />
     </div>
   );
---
+++ Client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light" style={{ backgroundColor: "#e3f2fd" }}>
+        Take home test
+      </nav>
+    )
+  }
+}
+
+export default Navbar;
